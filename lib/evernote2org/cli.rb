99291508ff20cb586b4ac6ae755e7a522ebe4148
Liu Xiang <liuxiang@ktjr.com>
2018-08-07 18:56:00 +0800

    Add option parser

--- lib/evernote2org/cli.rb
+++ lib/evernote2org/cli.rb
@@ -1,14 +1,32 @@
 require 'evernote2org/enex'
+require 'optparse'
 require 'fileutils'
 
 module Evernote2org
   module Cli
     class << self
       def start
-        enex = Enex.parse(ARGV[0])
-        out_dir = ARGV[1]
-        FileUtils.mkdir_p(out_dir)
-        enex.export_to(out_dir)
+        options = build_options!
+        enex = Enex.parse(options[:input])
+        FileUtils.mkdir_p(options[:output])
+        enex.export_to(options[:output])
+      end
+
+      def build_options!
+        options = {}
+        OptionParser.new do |opts|
+          opts.banner = "Usage: evernote2org -f [.enex file] -o [output.org]"
+
+          opts.on("-fFILE", "--file=FILE", "input .enex file") do |f|
+            options[:input] = f
+          end
+
+          opts.on("-oOUT", "--out=OUT", "output directory") do |o|
+            options[:output] = o
+          end
+        end.parse!
+
+        options
       end
     end
   end
